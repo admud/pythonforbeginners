09ad9de3bc3442bb85040008ff50a451cb40ff4d
muhdahmaddahlan <mudmud.muhd@gmail.com>
2019-01-23 17:08:02 +0800

    variables

---
+++ app.py
@@ -0,0 +1,10 @@
+ # print("hello lul")
+
+# use py -3 for python 3
+# use py -2 for python 2
+# or use special comment at the top of script
+
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/   |")
